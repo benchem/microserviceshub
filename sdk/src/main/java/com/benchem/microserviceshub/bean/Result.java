ff73e748479429dfc9f6258e48cb61f4058c13aa
BenChan@OfficePC <chenjiabin@lonntec.com>
2018-03-02 22:54:44 +0800

    调整返回值包装

--- sdk/src/main/java/com/benchem/microserviceshub/bean/Result.java
+++ sdk/src/main/java/com/benchem/microserviceshub/bean/Result.java
@@ -1,4 +1,44 @@
 package com.benchem.microserviceshub.bean;
 
+import com.benchem.microserviceshub.lang.SystemStateCode;
+
 public class Result {
+    Integer statecode;
+    String errmsg;
+    Object result;
+
+    public Result(Integer statecode, String errmsg) {
+        this.statecode = statecode;
+        this.errmsg = errmsg;
+    }
+
+    public Result(Object result) {
+        this.statecode = SystemStateCode.OK.getCode();
+        this.errmsg = SystemStateCode.OK.getMessage();
+        this.result = result;
+    }
+
+    public Integer getStatecode() {
+        return statecode;
+    }
+
+    public void setStatecode(Integer statecode) {
+        this.statecode = statecode;
+    }
+
+    public String getErrmsg() {
+        return errmsg;
+    }
+
+    public void setErrmsg(String errmsg) {
+        this.errmsg = errmsg;
+    }
+
+    public Object getResult() {
+        return result;
+    }
+
+    public void setResult(Object result) {
+        this.result = result;
+    }
 }
--- sdk/src/main/java/com/benchem/microserviceshub/sdk/MicroServiceResponseAdvisor.java
+++ sdk/src/main/java/com/benchem/microserviceshub/sdk/MicroServiceResponseAdvisor.java
@@ -2,40 +2,53 @@ package com.benchem.microserviceshub.sdk;
 
 
 import com.alibaba.fastjson.JSONObject;
+import com.benchem.microserviceshub.bean.Result;
 import com.benchem.microserviceshub.lang.MicroServiceException;
 import com.benchem.microserviceshub.lang.StateCode;
 import com.benchem.microserviceshub.lang.SystemStateCode;
+import org.springframework.core.MethodParameter;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 @ControllerAdvice
-public class MicroServiceExceptionHandler {
+public class MicroServiceResponseAdvisor implements ResponseBodyAdvice<Object> {
 
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<Object> handleUnhandleException(Exception ex, WebRequest request) {
-        JSONObject result = new JSONObject();
-        result.put("statecode", SystemStateCode.SYSTEM_ERROR.getCode());
-        result.put("errmsg", SystemStateCode.SYSTEM_ERROR.getMessage());
         String errMsg = ex.getMessage();
         if(errMsg != null && !errMsg.isEmpty()){
-            result.put("innermsg", errMsg);
+            ex.printStackTrace();
         }
+        Result result = new Result(SystemStateCode.SYSTEM_ERROR.getCode(), SystemStateCode.SYSTEM_ERROR.getMessage());
         return new ResponseEntity(result, HttpStatus.OK);
     }
 
     @ExceptionHandler(value = MicroServiceException.class)
     public ResponseEntity<Object> handleException(MicroServiceException ex, WebRequest request) {
-        StateCode code = ex.getStateCode();
-        JSONObject result = new JSONObject();
-        result.put("statecode", code.getCode());
-        result.put("errmsg", code.getMessage());
-        String errMsg = ex.getMessage();
-        if(errMsg != null && !errMsg.isEmpty()){
-            result.put("innermsg", errMsg);
-        }
+        StateCode stateCode = ex.getStateCode();
+        Result result = new Result(stateCode.getCode(), stateCode.getMessage());
         return new ResponseEntity(result, HttpStatus.OK);
+    }
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType,
+                                  Class<? extends HttpMessageConverter<?>> selectedConverterType,
+                                  ServerHttpRequest request, ServerHttpResponse response) {
+        if(body instanceof Result) return body;
+        return new Result(body);
     }
 }

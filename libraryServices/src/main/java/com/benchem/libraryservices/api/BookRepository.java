d34d776af05f6dce4e6634d5d54637ccbac9df1a
BenChan@OfficePC <chenjiabin@lonntec.com>
2018-03-07 09:15:31 +0800

    clear code

--- libraryServices/src/main/java/com/benchem/libraryservices/api/BookRepository.java
+++ libraryServices/src/main/java/com/benchem/libraryservices/api/BookRepository.java
@@ -2,11 +2,8 @@ package com.benchem.libraryservices.api;
 
 import com.benchem.libraryservices.bean.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.UUID;
 
 public interface BookRepository extends JpaRepository<Book, String> {
     List<Book> findByName(String bookName);
